4f5ce0e28ef530870eaf9d74ce9dc0ab87ba58db
Cavin <cavinlarry001@gmail.com>
2025-01-08 20:03:34 +0300

    Solved step 2

--- src/Main.kt
+++ src/Main.kt
@@ -1,10 +1,27 @@
 fun main() {
     // Write your solution here
-    println("Prices:")
-    println("bubblegum: $2")
-    println("Toffee: $0.2")
-    println("Ice cream: $5")
-    println("Milk chocolate: $4")
-    println("Doughnut: $2.5")
-    println("Pancake: $3.2")
+
+    // Stage 1 printing all the monthly earnings
+
+    // items declaration and initialization with earning
+    val bubblegum = 2002
+    val toffee = 118
+    val iceCream = 2250
+    val milkChocolate = 1680
+    val doughnut = 1075
+    val pancake = 80
+
+    // calculating the total monthly earning
+    val income = (bubblegum + toffee + iceCream + milkChocolate + doughnut + pancake)
+
+    // printing the outputs
+    println("Earned amount:")
+    println("Bubblegum: $$bubblegum")
+    println("Toffee: $$toffee")
+    println("Ice cream: $$iceCream")
+    println("Milk chocolate: $$milkChocolate")
+    println("Doughnut: $$doughnut")
+    println("Pancake: $$pancake")
+    println()
+    println("Income: $$income")
 }
